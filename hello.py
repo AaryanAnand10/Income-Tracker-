1c7b736caba1fcdab61e8393ed406d1d5eb8dbc5
Chaitanya Negi <118297902+GeekyChaitanya@users.noreply.github.com>
2023-12-20 23:51:33 +0530

    updating hello.py file
    added main function in python file to organize and manage functions better

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
-expense=input("Enter your daily expenses : ")
-salary=input("Enter your salary : ")
-exp=int(expense)
-sal=int(salary)
-print("Your remaining balance is",sal-exp,"only")
+def main():
+  expense=input("Enter your daily expenses : ")
+  salary=input("Enter your salary : ")
+  exp=int(expense)
+  sal=int(salary)
+  print("Your remaining balance is",sal-exp,"only")
+
+if __name__=="__main__":
+  main()
