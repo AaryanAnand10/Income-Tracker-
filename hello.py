46a34d4a1dfa7887402fde2856f47c7d24fa5399
Chaitanya Negi <118297902+GeekyChaitanya@users.noreply.github.com>
2023-12-20 23:41:13 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
-print("Hello")
+expense=input("Enter your daily expenses : ")
+salary=input("Enter your salary : ")
+exp=int(expense)
+sal=int(salary)
+print("Your remaining balance is",sal-exp,"only")
